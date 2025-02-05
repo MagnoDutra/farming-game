0c865599253bfce66de01204dc2d9a9fa16574a2
Magno <jmagno.dutra@hotmail.com>
2025-02-05 15:39:05 -0300

    animacao do player e movimentacao feita

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,33 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerController : MonoBehaviour
+{
+    private Rigidbody2D rb;
+    private Animator anim;
+
+    [SerializeField] private InputActionReference moveInput;
+    [SerializeField] private float moveSpeed = 5;
+
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        anim = GetComponentInChildren<Animator>();
+    }
+
+    void Update()
+    {
+        rb.linearVelocity = moveInput.action.ReadValue<Vector2>() * moveSpeed;
+
+        anim.SetFloat("speed", rb.linearVelocity.magnitude);
+
+        if (rb.linearVelocity.x < 0f)
+        {
+            transform.localScale = new Vector3(-1, 1, 1);
+        }
+        else if (rb.linearVelocity.x > 0f)
+        {
+            transform.localScale = Vector3.one;
+        }
+    }
+}
