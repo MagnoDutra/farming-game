1e92a3e958e60f6ae5e6bfe6bb8a3dffec5bbc7b
Magno <jmagno.dutra@hotmail.com>
2025-02-26 16:57:29 -0300

    grid size crops

--- Assets/Scripts/GridController.cs
+++ Assets/Scripts/GridController.cs
@@ -5,6 +5,7 @@ public class GridController : MonoBehaviour
     [SerializeField] Transform minPoint, maxPoint;
     [SerializeField] GrowBlock baseGridBlock;
 
+    private Vector2Int gridSize;
     private Vector3 offset;
 
     void Start()
@@ -24,6 +25,9 @@ public class GridController : MonoBehaviour
         minPoint.position = new Vector3(Mathf.Round(minPoint.position.x), Mathf.Round(minPoint.position.y), 0);
         maxPoint.position = new Vector3(Mathf.Round(maxPoint.position.x), Mathf.Round(maxPoint.position.y), 0);
 
-        Instantiate(baseGridBlock, minPoint.position + offset, Quaternion.identity);
+        Vector3 startPos = minPoint.position + offset;
+
+        gridSize = new Vector2Int(Mathf.RoundToInt(maxPoint.position.x - minPoint.position.x),
+            Mathf.RoundToInt(maxPoint.position.y - minPoint.position.y));
     }
 }
